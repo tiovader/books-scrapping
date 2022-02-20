851037878c54bebeae833f7c29f7dc60a95f5b2d
Emanuel Luis <emanuel.filho08@gmail.com>
2022-02-19 22:19:05 -0300

    refatoração e criação da tabela category, além de herança de tools.Table

--- src/database/models.py
+++ src/database/models.py
@@ -1,25 +1,76 @@
-from sqlalchemy.engine import Engine
-from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+from .tools import Table
+
+Base: DeclarativeMeta = declarative_base()
 
 
-def init_db(engine: Engine) -> None:
-    Base.metadata.create_all(engine)
-
-
-Base = declarative_base()
-
-
-class Book(Base):
-    __tablename__ = 'book'
+class Book(Base, Table):
+    __tablename__ = 'Book'
 
     id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, ForeignKey('Category.id'))
     name = Column(String(255))
-    category = Column(String(255), nullable=False)
+    category = relationship('Category', backref='Category.name',
+                            primaryjoin='Book.category_id==Category.id')
     price = Column(Float, nullable=False)
     stock = Column(Integer)
     rating = Column(Integer)
-    
+    description = Column(String(1700))
+
     def __repr__(self) -> str:
-        return f'<Book(name={self.name}, category={self.category}, '\
-            f'price={self.price}, stock={self.stock})>'
+        return f'<Book(name={self.name}, category={self.category}, price={self.price}, stock={self.stock}, rating={self.rating}, description={self.description[:25]}...)>'
+
+    @classmethod
+    def handler(cls, *objects: dict):
+        query = cls.to_tuple()
+        to_create = []
+
+        for obj in objects:
+            category = obj.get('category')
+            del obj['category']
+
+            category_id = obj['category_id'] = Category.mapping[category]
+            name = obj.get('name')
+
+            if (name, category_id) in query:
+                cls.update(where=dict(name=name, category_id=category_id),
+                           **obj)
+            else:
+                to_create.append(obj)
+
+        cls.create(*to_create)
+
+    @classmethod
+    def to_tuple(cls):
+        query = cls.read()
+        return [(book.name, book.category) for book in query]
+
+
+class Category(Base, Table):
+    __tablename__ = 'Category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
+
+    def __repr__(self) -> str:
+        return f'<Category(name={self.name})>'
+
+    @classmethod
+    def handler(cls, *objects):
+        query = cls.to_list()
+        cls.create(*filter(lambda o: o['name'] not in query, objects))
+
+    @classmethod
+    def to_list(cls):
+        return [category.name 
+                for category in cls.query or cls.read()]
+
+    @classmethod
+    @property
+    def mapping(cls):
+        return {category.name: category.id 
+                for category in cls.query or cls.read()}
